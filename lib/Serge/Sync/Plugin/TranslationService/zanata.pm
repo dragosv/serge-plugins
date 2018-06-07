63ccdbc8f83619c1c2d1219cf44e2e825472c74d
Dragos Varovici <dvarovici.work@gmail.com>
2018-06-07 09:15:32 -0600

    Zanata cache support

--- lib/Serge/Sync/Plugin/TranslationService/zanata.pm
+++ lib/Serge/Sync/Plugin/TranslationService/zanata.pm
@@ -8,7 +8,7 @@ use strict;
 
 use Serge::Util qw(subst_macros);
 
-our $VERSION = '0.9';
+our $VERSION = '0.10';
 
 sub name {
     return 'Zanata translation server (http://zanata.org/) synchronization plugin';
@@ -22,9 +22,20 @@ sub init {
     $self->{optimizations} = 1;
 
     $self->merge_schema({
+        # Project configuration file, eg zanata.xml
         project_config => 'STRING',
+        # User configuration, eg /home/user/.config/zanata.ini
         user_config => 'STRING',
-        push_type => 'STRING'
+        # Type of push to perform on the server:
+        #   source  pushes source documents only
+        #   both (default) pushes both source and translation documents
+        push_type => 'STRING',
+        # The base directory for storing zanata cache files. Default is current directory.
+        cache_dir => 'STRING',
+        # Whether to use an Entity cache when fetching documents.
+        use_cache => 'BOOLEAN',
+        # Whether to purge the cache before performing the pull operation
+        purge_cache => 'BOOLEAN'
     });
 }
 
@@ -36,9 +47,16 @@ sub validate_data {
     $self->{data}->{project_config} = subst_macros($self->{data}->{project_config});
     $self->{data}->{user_config} = subst_macros($self->{data}->{user_config});
     $self->{data}->{push_type} = subst_macros($self->{data}->{push_type});
+    $self->{data}->{cache_dir} = subst_macros($self->{data}->{cache_dir});
+    $self->{data}->{use_cache} = subst_macros($self->{data}->{use_cache});
+    $self->{data}->{purge_cache} = subst_macros($self->{data}->{purge_cache});
 
     die "'project_config' not defined" unless defined $self->{data}->{project_config};
     die "'project_config', which is set to '$self->{data}->{project_config}', does not point to a valid file.\n" unless -f $self->{data}->{project_config};
+
+    if (defined $self->{data}->{cache_dir}) {
+        die "'cache_dir', which is set to '$self->{data}->{cache_dir}', does not point to a valid dir.\n" unless -d $self->{data}->{cache_dir};
+    }
 
     if (defined $self->{data}->{user_config}) {
         die "'user_config', which is set to '$self->{data}->{user_config}', does not point to a valid file.\n" unless -f $self->{data}->{user_config};
@@ -89,7 +107,31 @@ sub get_zanata_locale {
 sub pull_ts {
     my ($self, $langs) = @_;
 
-    return $self->run_zanata_cli('pull', $langs);
+    my $action = 'pull';
+
+    if (defined $self->{data}->{cache_dir}) {
+        $action .= ' --cache-dir '.$self->{data}->{cache_dir};
+    }
+
+    if (defined $self->{data}->{use_cache}) {
+        $action .= ' --use-cache '.$self->to_boolean($self->{data}->{use_cache});
+    }
+
+    if (defined $self->{data}->{purge_cache}) {
+        $action .= ' --purge-cache '.$self->to_boolean($self->{data}->{purge_cache});
+    }
+
+    return $self->run_zanata_cli($action, $langs);
+}
+
+sub to_boolean {
+    my ($self, $boolean) = @_;
+
+    if ($boolean) {
+        return 'true';
+    }
+
+    return 'false';
 }
 
 sub push_ts {
