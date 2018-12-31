55ae0b8dbdd6950e9287dce78e3646cc54cb8934
Dragos Varovici <dvarovici.work@gmail.com>
2018-12-31 08:56:48 -0700

    Dzil command error checking

--- build-bin/dzil-plugins.pl
+++ build-bin/dzil-plugins.pl
@@ -6,21 +6,28 @@ use Template;
 my ($command) = @ARGV;
 
 if (not defined $command) {
-  die "command undefined\n";
+    die "command undefined\n";
 }
 
-my $template = Template->new;
+my $template = Template->new || die Template->error(), "\n";
+
 my @dirs = glob('lib/Serge/Sync/Plugin/TranslationService/*.pm');
 
 foreach my $dir (@dirs) {
-  my ($module_name) = ($dir =~ m/.*\/(.*?)\.pm/);
-  print "$module_name\n";
+    my ($module_name) = ($dir =~ m/.*\/(.*?)\.pm/);
 
-  $template->process('dist.ini-plugins', {
-      api_name => $module_name
-    },
-    'dist.ini'
-  );
+    print "$module_name\n";
 
-  print `dzil $command`;
+    $template->process('dist.ini-plugins', {
+	  api_name => $module_name
+	},
+	'dist.ini') || die $template->error(), "\n";
+
+    my $result = `dzil $command`;
+
+    print $result;
+
+    my $error_code = unpack 'c', pack 'C', $? >> 8; # error code
+
+    die "\nExit code: $error_code; last error: $!\n" if $error_code != 0;
 }
