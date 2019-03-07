255fb51bcd1ffbc5e8b3c0d326ef90434d317fcc
Dragos Varovici <dvarovici.work@gmail.com>
2019-03-06 17:49:02 -0700

    Bumped PhraseApp version

--- lib/Serge/Sync/Plugin/TranslationService/phraseapp.pm
+++ lib/Serge/Sync/Plugin/TranslationService/phraseapp.pm
@@ -8,7 +8,7 @@ use strict;
 use Serge::Util qw(subst_macros);
 use version;
 
-our $VERSION = qv('0.902.0');
+our $VERSION = qv('0.902.1');
 
 sub name {
     return 'PhraseApp translation software (https://phraseapp.com) synchronization plugin';
